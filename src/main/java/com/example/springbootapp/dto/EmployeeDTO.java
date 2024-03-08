6709a04c064c792f61d216f9ad09671e79dc3d1a
Larisa-sh <69165735+Larisa-sh@users.noreply.github.com>
2024-03-08 21:51:22 +0300

    feat: Swagger was added to the project (#5)
    * feat: Swagger was added to the project
    
    * fix: More annotations were added to enrich API description.
    EmployeeDTO is returned in controller in save and update methods.
    
    * fix: Required property was added to the DTO fields and for all ID in controller.

--- src/main/java/com/example/springbootapp/dto/EmployeeDTO.java
+++ src/main/java/com/example/springbootapp/dto/EmployeeDTO.java
@@ -2,15 +2,45 @@ package com.example.springbootapp.dto;
 
 import com.example.springbootapp.entity.Gender;
 import com.example.springbootapp.entity.JobTitle;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.Date;
 
-public record EmployeeDTO(Long employeeId,
+@Schema(description = "Employee Data Transfer Object")
+public record EmployeeDTO(@Schema(accessMode = Schema.AccessMode.READ_ONLY,
+                                  description = "Employee ID")
+                          Long employeeId,
+
+                          @Schema(description = "First name",
+                                  example = "Mark",
+                                  requiredMode = Schema.RequiredMode.REQUIRED)
                           String firstName,
+
+                          @Schema(description = "Last name",
+                                  example = "Stone",
+                                  requiredMode = Schema.RequiredMode.REQUIRED)
                           String lastName,
+
+                          @Schema(description = "ID of employee's department",
+                                  example = "2",
+                                  requiredMode = Schema.RequiredMode.REQUIRED)
                           int departmentId,
+
+                          @Schema(description = "Job title",
+                                  example = "PROGRAMMER",
+                                  requiredMode = Schema.RequiredMode.REQUIRED)
                           JobTitle jobTitle,
+
+                          @Schema(description = "Gender",
+                                  example = "MALE",
+                                  requiredMode = Schema.RequiredMode.REQUIRED)
                           Gender gender,
+
+                          @Schema(description = "Date of birth. Employee must be elder than 18 years.",
+                                  example = "2024-03-06",
+                                  format = "date",
+                                  type = "string",
+                                  requiredMode = Schema.RequiredMode.REQUIRED)
                           Date dateOfBirth) {
 
     public boolean isAtLeastOneParamNull(EmployeeDTO employeeDTO) {
